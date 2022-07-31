090750412e90f619297e914d1b22adc75f2f6195
Notnex <damir180999@gmail.com>
2022-07-31 18:32:23 +0300

    31.07.22

---
+++ app/src/main/java/com/example/tiktokplus/FloatingWindowGFG.kt
@@ -0,0 +1,126 @@
+package com.example.tiktokplus;
+
+import android.app.Service
+import android.content.Intent
+import android.graphics.PixelFormat
+import android.os.Build
+import android.os.IBinder
+import android.view.*
+import android.view.View.OnTouchListener
+import android.widget.Button
+import com.example.tiktokplus.R
+
+class FloatingWindowGFG : Service() {
+    //The reference variables for the
+    //ViewGroup, WindowManager.LayoutParams, WindowManager, Button, EditText classes are created
+    private var floatView: ViewGroup? = null
+    private var LAYOUT_TYPE = 0
+    private var floatWindowLayoutParam: WindowManager.LayoutParams? = null
+    private var windowManager: WindowManager? = null
+    private val maximizeBtn: Button? = null
+
+    //As FloatingWindowGFG inherits Service class, it actually overrides the onBind method
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        //The screen height and width are calculated, cause
+        //the height and width of the floating window is set depending on this
+        val metrics = applicationContext.resources.displayMetrics
+        val width = metrics.widthPixels
+        val height = metrics.heightPixels
+
+        //To obtain a WindowManager of a different Display,
+        //we need a Context for that display, so WINDOW_SERVICE is used
+        windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
+
+        //A LayoutInflater instance is created to retrieve the LayoutInflater for the floating_layout xml
+        val inflater = baseContext.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        //inflate a new view hierarchy from the floating_layout xml
+        floatView = inflater.inflate(R.layout.floating_layout, null) as ViewGroup?
+
+        //The Buttons and the EditText are connected with
+        //the corresponding component id used in floating_layout xml file
+        //maximizeBtn = floatView.findViewById(R.id.buttonMaximize);
+
+        //WindowManager.LayoutParams takes a lot of parameters to set the
+        //the parameters of the layout. One of them is Layout_type.
+        LAYOUT_TYPE = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //If API Level is more than 26, we need TYPE_APPLICATION_OVERLAY
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        } else {
+            //If API Level is lesser than 26, then we can use TYPE_SYSTEM_ERROR,
+            //TYPE_SYSTEM_OVERLAY, TYPE_PHONE, TYPE_PRIORITY_PHONE. But these are all
+            //deprecated in API 26 and later. Here TYPE_TOAST works best.
+            WindowManager.LayoutParams.TYPE_TOAST
+        }
+
+        //Now the Parameter of the floating-window layout is set.
+        //1) The Width of the window will be 55% of the phone width.
+        //2) The Height of the window will be 58% of the phone height.
+        //3) Layout_Type is already set.
+        //4) Next Parameter is Window_Flag. Here FLAG_NOT_FOCUSABLE is used. But
+        //problem with this flag is key inputs can't be given to the EditText.
+        //This problem is solved later.
+        //5) Next parameter is Layout_Format. System chooses a format that supports translucency by PixelFormat.TRANSLUCENT
+        floatWindowLayoutParam = WindowManager.LayoutParams(
+            width, (height * 0.06f).toInt(),
+            LAYOUT_TYPE,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+            PixelFormat.TRANSLUCENT
+        )
+
+        //The Gravity of the Floating Window is set. The Window will appear in the center of the screen
+        floatWindowLayoutParam!!.gravity = Gravity.BOTTOM
+        //X and Y value of the window is set
+        //floatWindowLayoutParam.x = 0;
+        //floatWindowLayoutParam.y = 1000;
+
+        //The ViewGroup that inflates the floating_layout.xml is
+        //added to the WindowManager with all the parameters
+        windowManager!!.addView(floatView, floatWindowLayoutParam)
+
+
+
+        //Another feature of the floating window is, the window is movable.
+        //The window can be moved at any position on the screen.
+        floatView!!.setOnTouchListener(object : OnTouchListener {
+            val floatWindowLayoutUpdateParam: WindowManager.LayoutParams = floatWindowLayoutParam as WindowManager.LayoutParams
+            var x = 0.0
+            var y = 0.0
+            var px = 0.0
+            var py = 0.0
+            override fun onTouch(v: View, event: MotionEvent): Boolean {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        x = floatWindowLayoutUpdateParam.x.toDouble()
+                        y = floatWindowLayoutUpdateParam.y.toDouble()
+                        //returns the original raw X coordinate of this event
+                        px = event.rawX.toDouble()
+                        //returns the original raw Y coordinate of this event
+                        py = event.rawY.toDouble()
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        floatWindowLayoutUpdateParam.x = (x + event.rawX - px).toInt()
+                        floatWindowLayoutUpdateParam.y = (y + event.rawY - py).toInt()
+
+                        //updated parameter is applied to the WindowManager
+                        windowManager!!.updateViewLayout(floatView, floatWindowLayoutUpdateParam)
+                    }
+                }
+                return false
+            }
+        })
+    }
+
+    //It is called when stopService() method is called in MainActivity
+    override fun onDestroy() {
+        super.onDestroy()
+        stopSelf()
+        //Window is removed from the screen
+        windowManager!!.removeView(floatView)
+    }
+}
---
+++ app/src/main/java/com/example/tiktokplus/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.example.tiktokplus
+
+import android.app.ActivityManager
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var dialog: AlertDialog? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        startService(Intent(this@MainActivity, FloatingWindowGFG::class.java))
+
+        //setContentView(R.layout.activity_main);
+        val launchIntent = packageManager.getLaunchIntentForPackage("com.zhiliaoapp.musically")
+        if (launchIntent != null) {
+            startActivity(launchIntent);//null pointer check in case package name was not found
+        }
+
+
+        /*ActivityManager activityManager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.AppTask> tasks = Objects.requireNonNull(activityManager).getAppTasks();
+        for (ActivityManager.AppTask task : tasks) {
+            String taskValue = "Application executed : " + task.getTaskInfo().baseActivity.toShortString() + "\tID: " + task.getTaskInfo().taskId + "\n";
+            Toast.makeText(this, taskValue, Toast.LENGTH_LONG).show();
+        }*/
+
+
+
+        //If the app is started again while the floating window service is running
+        //then the floating window service will stop
+        if (isMyServiceRunning()) {
+            //onDestroy() method in FloatingWindowGFG class will be called here
+            stopService(Intent(this@MainActivity, FloatingWindowGFG::class.java))
+        }
+
+        startService(Intent(this@MainActivity, FloatingWindowGFG::class.java))
+
+    }
+
+
+
+    private fun isMyServiceRunning(): Boolean {
+        //The ACTIVITY_SERVICE is needed to retrieve a ActivityManager for interacting with the global system
+        //It has a constant String value "activity".
+        val manager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        //A loop is needed to get Service information that are currently running in the System.
+        //So ActivityManager.RunningServiceInfo is used. It helps to retrieve a
+        //particular service information, here its this service.
+        //getRunningServices() method returns a list of the services that are currently running
+        //and MAX_VALUE is 2147483647. So at most this many services can be returned by this method.
+        for (service in manager.getRunningServices(Int.MAX_VALUE)) {
+            //If this service is found as a running, it will return true or else false.
+            if (FloatingWindowGFG::class.java.getName() == service.service.className) {
+                return true
+            }
+        }
+        return false
+    }
+    private fun requestOverlayDisplayPermission() {
+        //An AlertDialog is created
+        val builder = AlertDialog.Builder(this)
+        //This dialog can be closed, just by taping anywhere outside the dialog-box
+        builder.setCancelable(true)
+        //The title of the Dialog-box is set
+        builder.setTitle("Screen Overlay Permission Needed")
+        //The message of the Dialog-box is set
+        builder.setMessage("Enable 'Display over other apps' from System Settings.")
+        //The event of the Positive-Button is set
+        builder.setPositiveButton(
+            "Open Settings"
+        ) { dialog, which -> //The app will redirect to the 'Display over other apps' in Settings.
+            //This is an Implicit Intent. This is needed when any Action is needed to perform, here it is
+            //redirecting to an other app(Settings).
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            //This method will start the intent. It takes two parameter, one is the Intent and the other is
+            //an requestCode Integer. Here it is -1.
+            startActivityForResult(intent, RESULT_OK)
+        }
+        dialog = builder.create()
+        //The Dialog will show in the screen
+        dialog!!.show()
+    }
+
+    private fun checkOverlayDisplayPermission(): Boolean {
+        //Android Version is lesser than Marshmallow or the API is lesser than 23
+        //doesn't need 'Display over other apps' permission enabling.
+        return if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            //If 'Display over other apps' is not enabled it will return false or else true
+            Settings.canDrawOverlays(this)
+        } else {
+            true
+        }
+    }
+}
